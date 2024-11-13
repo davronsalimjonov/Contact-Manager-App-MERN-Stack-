676d82314459252ef22e9ac02d293657231e51ef
Muxammadaziz <muxammadazizramziddinov@gmail.com>
2024-11-13 18:13:28 +0500

    Select compoment updated

--- src/components/UI/atoms/Form/Select/index.jsx
+++ src/components/UI/atoms/Form/Select/index.jsx
@@ -1,5 +1,20 @@
 import ReactSelect, { components } from 'react-select'
 import cls from './Select.module.scss';
+import { CloseIcon } from '../../icons';
+
+const ClearIndicator = (props) => {
+    return (
+        <components.ClearIndicator {...props} className={cls.closeIcon}>
+            <CloseIcon />
+        </components.ClearIndicator>
+    );
+};
+
+const DropdownIndicator = (props) => {
+    return props.selectProps.isClearable && props.hasValue ? null : (
+        <components.DropdownIndicator {...props} className={cls.arrowIcon} />
+    );
+};
 
 const Select = ({
     onChange,
@@ -11,7 +26,9 @@ const Select = ({
     return (
         <ReactSelect
             components={{
-                Menu: (props) => <components.Menu {...props} className={cls.menu} />
+                Menu: (props) => <components.Menu {...props} className={cls.menu} />,
+                ClearIndicator,
+                DropdownIndicator
             }}
             className={cls.select}
             options={options}
--- src/components/pages/MyStudents/index.jsx
+++ src/components/pages/MyStudents/index.jsx
@@ -8,7 +8,7 @@ import StudentsSearchBar from '@/components/UI/organisms/StudentsSearchBar';
 import cls from './MyStudents.module.scss';
 
 const MyStudents = () => {
-    const [filter, setFilter] = useState()
+    const [filter, setFilter] = useState({})
     const { data: groups } = useGetGroups()
     const { ref, data: students, isLoading: isLoadingStudents } = useGetStudents(filter)
 
@@ -24,7 +24,7 @@ const MyStudents = () => {
         <div className={cls.page}>
             <Tabs options={tabOptions} onChange={group => setFilter(state => ({ ...state, group }))} />
             <StudentsSearchBar
-                onChangeStatus={(status) => setFilter(state => ({ ...state, status }))}
+                onChangeStatus={(status) => setFilter(state => ({ ...state, status: status?.value }))}
                 onChangeFirstName={e => setFilter(state => ({ ...state, firstName: e.target.value?.trim() }))}
                 onChangeLastName={e => setFilter(state => ({ ...state, lastName: e.target.value?.trim() }))}
                 onChangePhone={e => setFilter(state => ({ ...state, phone: sanitizePhoneNumber(e.target.value) }))}
