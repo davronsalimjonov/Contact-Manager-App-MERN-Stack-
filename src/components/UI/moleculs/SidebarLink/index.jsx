1218c8caaf676868d27ffdd730d6354bf984c3d8
Muxammadaziz <muxammadazizramziddinov@gmail.com>
2024-11-28 11:08:34 +0500

    Sidebar children link added

--- src/components/UI/moleculs/SidebarLink/index.jsx
+++ src/components/UI/moleculs/SidebarLink/index.jsx
@@ -13,28 +13,33 @@ const SidebarLink = ({
     children
 }) => {
     const [isOpenDropDown, setIsOpenDropdown] = useState(false)
-
+    const hasChildren = children?.length > 0
     return (
-        <NavLink to={children?.length ? children[0]?.link : to} className={({ isActive }) => cn(cls.link, isActive ? cls.active : '')}>
+        <NavLink 
+            to={hasChildren ? children[0]?.link : to} 
+            className={({ isActive }) => cn(cls.link, isActive ? cls.active : '')}
+        >
             <MenuItem
-                onClick={() => setIsOpenDropdown(state => !state)}
+                onClick={hasChildren ? () => setIsOpenDropdown(state => !state) : undefined}
                 className={''}
                 label={label}
                 icon={icon}
                 isOpen={isOpen}
-                preffix={children?.length ? <RightArrowIcon style={{ transform: isOpenDropDown ? 'rotate(-90deg)' : 'rotate(90deg)', transition: 'all 0.3s' }} /> : ''}
+                preffix={hasChildren && <RightArrowIcon style={{ transform: isOpenDropDown ? 'rotate(-90deg)' : 'rotate(90deg)', transition: 'all 0.3s' }} />}
             />
-            <div className={cn(cls.dropdown, (isOpenDropDown && isOpen) ? cls.openDropdown : '')}>
-                {children?.length > 0 && children.map(link => (
-                    <NavLink
-                        key={link.id}
-                        className={({ isActive }) => cn(cls.dropdown__link, isActive ? cls.dropdown__link__active : '')}
-                        to={link.link}
-                    >
-                        {link.label}
-                    </NavLink>
-                ))}
-            </div>
+            {hasChildren && (
+                <div className={cn(cls.dropdown, (isOpenDropDown && isOpen) ? cls.openDropdown : '')}>
+                    {children.map(link => (
+                        <NavLink
+                            key={link.id}
+                            className={({ isActive }) => cn(cls.dropdown__link, isActive ? cls.dropdown__link__active : '')}
+                            to={link.link}
+                        >
+                            {link.label}
+                        </NavLink>
+                    ))}
+                </div>
+            )}
         </NavLink>
     );
 }
