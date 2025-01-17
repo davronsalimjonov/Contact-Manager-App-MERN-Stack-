8b709938226338246e015837ec8fe0f3048f6df9
Muxammadaziz <muxammadazizramziddinov@gmail.com>
2025-01-17 19:57:31 +0500

    Setting validation updated

--- src/schemas/employee.js
+++ src/schemas/employee.js
@@ -6,6 +6,17 @@ export const employeeSchema = Yup.object().shape({
     lastName: Yup.string().required('Familiya kiritish majburiy'),
     phone: phoneNumberSchema.required("Telefon raqam majburiy"),
     gender: genderSchema,
-    birthday: Yup.string().nullable(),
+    birthday: Yup.date()
+    .nullable()
+    .test(
+        'min-age',
+        'Yosh 7 dan katta bo‘lishi kerak',
+        function (value) {
+            if (!value) return true; // Поле необязательное, пропускаем проверку для пустого значения.
+            const today = new Date();
+            const minDate = new Date(today.getFullYear() - 7, today.getMonth(), today.getDate());
+            return value <= minDate; // Проверяем, что дата рождения меньше или равна минимальной дате.
+        }
+    ),
     degree: Yup.string().required('Til bilish darajasini tanlang')
 })
