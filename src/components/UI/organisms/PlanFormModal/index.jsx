dc4b2d16f5a18f0585a9c8fd6144ac64252de629
Muxammadaziz <muxammadazizramziddinov@gmail.com>
2025-01-21 17:22:30 +0500

    Plan form created

---
+++ src/components/UI/organisms/PlanFormModal/index.jsx
@@ -0,0 +1,74 @@
+import Button from '../../atoms/Buttons/Button';
+import Dialog from '../../moleculs/Dialog';
+import FormInput from '../../moleculs/Form/FormInput';
+import cls from './PlanFormModal.module.scss';
+import { useForm } from 'react-hook-form';
+
+const PlanFormModal = ({
+    isOpen = false,
+    onClose
+}) => {
+    const { register, handleSubmit, formState: { isDirty, isSubmitting, errors } } = useForm()
+
+    function extractPrice(value) {
+        const numericValue = value.replace(/\D/g, "");
+        return parseInt(numericValue, 10) || 0;
+    }
+
+    const formatToCurrency = (inputValue) => {
+        const numericValue = inputValue.replace(/\D/g, "");
+        const formattedValue = new Intl.NumberFormat("ru-RU").format(numericValue);
+        return `${formattedValue} so'm`;
+    };
+
+    const handleChange = (e) => {
+        const input = e.target;
+        const rawValue = input.value;
+        const cursorPosition = input.selectionStart;
+
+        const formattedValue = formatToCurrency(rawValue);
+
+        e.target.value = (formattedValue);
+
+        const newCursorPosition = cursorPosition + 1 < formattedValue.length - 5 ? cursorPosition : formattedValue.length - 5;
+
+        setTimeout(() => {
+            input.setSelectionRange(newCursorPosition, newCursorPosition);
+        });
+    };
+
+    const handleSubmitForm = (data) => {
+        data.plan = extractPrice(data.plan)
+        onSubmit?.(data)
+    }
+
+    return (
+        <Dialog
+            isOpen={isOpen}
+            onClose={onClose}
+        >
+            <form className={cls.form} onSubmit={handleSubmit(handleSubmitForm)}>
+                <h2>Plan kiritish</h2>
+                <FormInput
+                    label='Plan kiriting'
+                    placeholder='100 000 000 so’m'
+                    onChange={handleChange}
+                    register={register('plan', {
+                        validate: (value) => extractPrice(value) >= 20_000_000 || "Minimal plan 20 000 000 so'm",
+                        onChange: handleChange
+                    })}
+                    error={errors?.plan?.message}
+                />
+                <Button
+                    type='submit'
+                    disabled={!isDirty}
+                    isLoading={isSubmitting}
+                >
+                    Kiritish
+                </Button>
+            </form>
+        </Dialog>
+    );
+}
+
+export default PlanFormModal;
