4a9be2d3b5787efd7d2371145a291c4bf9711076
Muhammadaziz <muxammadazizramziddinov@gmail.com>
2025-01-25 09:52:54 +0500

    Update index.jsx

--- src/components/UI/organisms/MaterialPreviewModal/index.jsx
+++ src/components/UI/organisms/MaterialPreviewModal/index.jsx
@@ -26,7 +26,7 @@ const MaterialPreviewModal = ({
                     {fileType === 'image' && <img className={cls.modal__image} src={fileUrl} alt="" />}
                     {fileType === 'video' && <video className={cls.modal__video} src={fileUrl} controls />}
                     {fileType === 'docs' && <iframe className={cls.modal__iframe} src={'https://view.officeapps.live.com/op/embed.aspx?src=' + encodeURIComponent(fileUrl)} frameBorder="0" />}
-                    {fileType === 'pdf' && <iframe className={cls.modal__iframe} src={'http://docs.google.com/gview?&embedded=true&url=' + encodeURIComponent(fileUrl)} frameBorder="0" />}
+                    {fileType === 'pdf' && <iframe className={cls.modal__iframe} src={'https://docs.google.com/gview?&embedded=true&url=' + encodeURIComponent(fileUrl)} frameBorder="0" />}
                     {fileType === 'audio' && <AudioPlayer title={fileName} url={fileUrl} />}
                 </div>
             </div>
