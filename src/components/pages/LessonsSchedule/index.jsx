79f3c6bd4880ee534c33effc42cdb4be4cc81734
Muxammadaziz <muxammadazizramziddinov@gmail.com>
2025-01-27 16:19:30 +0500

    Some changes

--- src/components/pages/LessonsSchedule/index.jsx
+++ src/components/pages/LessonsSchedule/index.jsx
@@ -8,7 +8,7 @@ import { ru } from "date-fns/locale";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 
 const locales = {
-  ru: ru,
+  // ru: ru,
 };
 
 const localizer = dateFnsLocalizer({
@@ -27,9 +27,39 @@ const currentDate = today.getDate();
 const events = [
   {
     title: "A2 level",
-    start: new Date(currentYear, currentMonth, currentDate, 10, 0),
-    end: new Date(currentYear, currentMonth, currentDate, 11, 45),
-    backgroundColor: "#e2ffe2",
+    start: new Date(currentYear, currentMonth, 27, 10, 0),
+    end: new Date(currentYear, currentMonth, 27, 11, 45),
+    color: "rgba(206, 242, 203, 1)",
+  },
+  {
+    title: "A2 level",
+    start: new Date(currentYear, currentMonth, 29, 10, 0),
+    end: new Date(currentYear, currentMonth, 29, 11, 45),
+    color: "rgba(206, 242, 203, 1)",
+  },
+  {
+    title: "A2 level",
+    start: new Date(currentYear, currentMonth + 1, 1, 10, 0),
+    end: new Date(currentYear, currentMonth + 1, 1, 11, 45),
+    color: "rgba(206, 242, 203, 1)",
+  },
+  {
+    title: "B1 level",  
+    start: new Date(currentYear, currentMonth, 28, 14, 0),
+    end: new Date(currentYear, currentMonth, 28, 16, 15),
+    color: "rgba(255, 189, 167, 1)",
+  },
+  {
+    title: "B1 level",
+    start: new Date(currentYear, currentMonth, 30, 22, 0),
+    end: new Date(currentYear, currentMonth, 30, 23, 0),
+    color: "rgba(255, 189, 167, 1)",
+  },
+  {
+    title: "B1 level",
+    start: new Date(currentYear, currentMonth, 31, 14, 30),
+    end: new Date(currentYear, currentMonth, 31, 16, 15),
+    color: "rgba(255, 189, 167, 1)",
   },
 ];
 
@@ -37,13 +67,13 @@ function MyCalendar() {
   const eventStyleGetter = (event) => {
     return {
       style: {
-        backgroundColor: "#d1fad1",
+        backgroundColor: event.color,
         color: "#1b1b1b",
-        borderRadius: "10px",
+        borderRadius: "6px",
         padding: "10px",
-        fontSize: "16px",
+        fontSize: "20px",
         fontWeight: "500",
-        border: "1px solid #b3e6b3",
+        border: "none",
         boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)",
         display: "flex",
         alignItems: "center",
@@ -65,36 +95,26 @@ function MyCalendar() {
     },
   };
 
-  const customSlotStyle = {
-    width: '80px',
-    height: '112px',
-    backgroundColor: '#f0f0f0',
-    border: '1px solid #ddd',
-    borderRadius: '5px',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-  };
+  const CustomTimeSlotWrapper = ({ value, ...props }) => {
+    const minutes = value.getMinutes();
+    const hours = value.getHours();
 
-  // Кастомизация слота
-  const customSlot = ({ date, children }) => {
+    let displayText = '';
+    if (minutes === 0) {
+      displayText = `${hours}:00`; // Часы
+    } else if (minutes === 15 || minutes === 30 || minutes === 45) {
+      displayText = `:${minutes}`; // Только минуты
+    }
+
     return (
-      <div style={customSlotStyle}>
-        {children}
+      <div className="rbc-time-slot">
+        {displayText && <span className="rbc-label">{displayText}</span>}
       </div>
     );
   };
 
   return (
-    <div style={{}}>
-      <style>
-        {`
-          /* Убираем границы между слотами */
-        //   .rbc-timeslot-group {
-        //     min-height: 80px;
-        //   }
-        `}
-      </style>
+    <div>
       <Calendar
         localizer={localizer}
         events={events}
@@ -111,15 +131,11 @@ function MyCalendar() {
         toolbar={false}
         formats={formats}
         components={{
-          timeSlot: ({ children }) => (
-            <div className="custom-time-slot">
-              {children}
-            </div>
-          ),
+          timeSlotWrapper: CustomTimeSlotWrapper
         }}
         dayPropGetter={(date) => {
           if (date.getDay() === 0) {
-            return { style: { display: "none" } }; // Скрываем воскресенье
+            return { style: { display: "none" } };
           }
           return {};
         }}
