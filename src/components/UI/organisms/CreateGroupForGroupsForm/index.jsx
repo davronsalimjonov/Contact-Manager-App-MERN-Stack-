a8d94d35ddd29b4cbaa278349a1d7fa15ccf15c1
Muxammadaziz <muxammadazizramziddinov@gmail.com>
2025-01-30 10:17:41 +0500

    commit

--- src/components/UI/organisms/CreateGroupForGroupsForm/index.jsx
+++ src/components/UI/organisms/CreateGroupForGroupsForm/index.jsx
@@ -26,64 +26,56 @@ const CreateGroupForGroupsForm = ({
     return (
         <Dialog isOpen={isOpen} onClose={onClose}>
             <form className={cls.MainMentorStudentsGroupTab__dialog} onSubmit={handleSubmit(onSubmit)}>
-                <div>
-                    <FormInput
-                        label='Guruh Nomi'
-                        placeholder='Guruh Nomini Kiriting'
-                        register={register('title', { required: 'Guruh Nomini Kiriting!' })}
-                        error={errors?.title?.message}
-                    />
-                </div>
-                <div onClick={(e) => e.stopPropagation()}>
-                    <FormSelect
-                        label='Status Tanlang'
-                        placeholder="Levelni Kiriting"
-                        options={[
-                            { value: 'A1', label: 'A1' },
-                            { value: 'A2', label: 'A2' },
-                            { value: 'B1', label: 'B1' },
-                            { value: 'B2', label: 'B2' },
-                            { value: 'C1', label: 'C1' },
-                            { value: 'C2', label: 'C2' },
-                        ]}
-                        isClearable
-                        isSearchable={true}
-                        control={control}
-                        name='level'
-                        rules={{ required: "Status Tanlang" }}
-                    />
-                </div>
-                <div onClick={(e) => e.stopPropagation()}>
-                    <FormSelect
-                        label='Asosiy Mentor'
-                        placeholder="Asosiy Mentorni Tanlang"
-                        isClearable
-                        isSearchable={true}
-                        options={mainMentorOptions}
-                        control={control}
-                        name='academyMentor'
-                        rules={{ required: "Asosiy Mentor Tanlang" }}
-                    />
-                </div>
-                <div onClick={(e) => e.stopPropagation()}>
-                    <FormSelect
-                        label='Call Mentorni Tanlang'
-                        placeholder="Nazoratchi Mentorni Tanlang"
-                        options={callMentorOptions}
-                        isClearable
-                        isSearchable={true}
-                        control={control}
-                        name='callMentor'
-                        rules={{ required: "Nazoratchi Mentor Tanlang" }}
-                    />
-                </div>
-                <div>
-                    <Button
-                        type='submit'
-                        isLoading={isSubmitting}
-                        className={cls.MainMentorStudentsGroupTab__dialog__btn}
-                    >Qo'shish</Button>
-                </div>
+                <FormInput
+                    label='Guruh Nomi'
+                    placeholder='Guruh Nomini Kiriting'
+                    register={register('title', { required: 'Guruh Nomini Kiriting!' })}
+                    error={errors?.title?.message}
+                />
+                <FormSelect
+                    label='Status Tanlang'
+                    placeholder="Levelni Kiriting"
+                    options={[
+                        { value: 'A1', label: 'A1' },
+                        { value: 'A2', label: 'A2' },
+                        { value: 'B1', label: 'B1' },
+                        { value: 'B2', label: 'B2' },
+                        { value: 'C1', label: 'C1' },
+                        { value: 'C2', label: 'C2' },
+                    ]}
+                    isClearable
+                    isSearchable={true}
+                    control={control}
+                    name='level'
+                    rules={{ required: "Status Tanlang" }}
+                />
+                <FormSelect
+                    label='Asosiy Mentor'
+                    placeholder="Asosiy Mentorni Tanlang"
+                    isClearable
+                    isSearchable={true}
+                    options={mainMentorOptions}
+                    control={control}
+                    name='academyMentor'
+                    rules={{ required: "Asosiy Mentor Tanlang" }}
+                />
+                <FormSelect
+                    label='Call Mentorni Tanlang'
+                    placeholder="Nazoratchi Mentorni Tanlang"
+                    options={callMentorOptions}
+                    isClearable
+                    isSearchable={true}
+                    control={control}
+                    name='callMentor'
+                    rules={{ required: "Nazoratchi Mentor Tanlang" }}
+                />
+                <Button
+                    type='submit'
+                    isLoading={isSubmitting}
+                    className={cls.MainMentorStudentsGroupTab__dialog__btn}
+                >
+                    Qo'shish
+                </Button>
             </form>
         </Dialog>
     )
