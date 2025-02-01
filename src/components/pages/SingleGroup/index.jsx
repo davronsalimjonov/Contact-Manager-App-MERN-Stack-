96dcb5e755cd1fe19d2fc2b11a17a4e77bb3e902
Muxammadaziz <muxammadazizramziddinov@gmail.com>
2025-02-01 11:17:29 +0500

    Some changes

--- src/components/pages/SingleGroup/index.jsx
+++ src/components/pages/SingleGroup/index.jsx
@@ -1,12 +1,14 @@
+import { useNavigate } from 'react-router-dom';
+import Pagination from '@/components/UI/moleculs/CustomPagination';
 import GroupInfoCard from '@/components/UI/organisms/GroupInfoCard';
 import GroupStudentsTable from '@/components/templates/GroupStudentsTable';
-import Pagination from '@/components/UI/moleculs/CustomPagination';
 import cls from './SingleGroup.module.scss';
 
 const SingleGroup = () => {
+    const navigate = useNavigate()
     return (
         <div className={cls.page}>
-            <GroupInfoCard />
+            <GroupInfoCard onClickCreateSchedule={() => navigate('lesson-schedule')} />
             <GroupStudentsTable />
             <Pagination pageCount={10} />
         </div>
