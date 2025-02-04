8efed31260f596ded4863eac9ea2e3e87bc7bb01
Muxammadaziz <muxammadazizramziddinov@gmail.com>
2025-02-04 15:24:28 +0500

    TransferStudentModal created

---
+++ src/components/UI/organisms/TransferStudentModal/index.jsx
@@ -0,0 +1,11 @@
+import cls from './TransferStudentModal.module.scss';
+
+const TransferStudentModal = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default TransferStudentModal;
