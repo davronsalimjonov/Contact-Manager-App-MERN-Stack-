7f804d06ac2ecb748f0acf05579feae638a63470
Davron Salimjonov <davronsalimjonov61@gmail.com>
2025-03-18 14:49:05 +0500

    small changes

--- src/components/UI/organisms/LessonInfoCard/index.jsx
+++ src/components/UI/organisms/LessonInfoCard/index.jsx
@@ -6,12 +6,13 @@ import Button from "../../atoms/Buttons/Button"
 import MediaPreviewer from "../../moleculs/MediaPreviewer"
 import { ArrowRightIcon, BookPlayIcon, PlusIcon } from "../../atoms/icons"
 import cls from "./LessonInfoCard.module.scss"
+import MediaMergingDialog from "@/components/templates/MediaMergingDialog"
 
 const LessonInfoCard = ({ lessonId = '' }) => {
     const navigate = useNavigate()
     const { data: lessonInfo } = useGetLessonInfo(lessonId)
     const [isOpenVideoModal, setIsOpenVideoModal] = useState(false)
-
+    const [videoPreview, setVideoPreview] = useState({ isOpen: false, status: null, url: '' })
     const lessonVideoUrl = lessonInfo?.video || ''
 
     return (
@@ -20,12 +21,14 @@ const LessonInfoCard = ({ lessonId = '' }) => {
                 <BookPlayIcon /> {lessonInfo?.title}
             </h2>
             <div className={cls.card__details}>
-                <button className={cls.card__details__btn} onClick={() => lessonVideoUrl ? setIsOpenVideoModal(true) : toast.error('Video topilmadi')}>
+                <button className={cls.card__details__btn} onClick={() => {
+                    lessonInfo.videoStatus === "merging" ? setVideoPreview({ isOpen: true, status: null, url: '' }) :  (lessonVideoUrl ? setIsOpenVideoModal(true) : toast.error('Video topilmadi')) && setVideoPreview({ isOpen: false, status: null, url: '' })
+                }}>
                     Videoni Ko'rish <ArrowRightIcon />
                 </button>
                 {lessonInfo?.lessonHomeTask ? (
-                    <button 
-                        onClick={() => navigate(`hometask/${lessonInfo?.lessonHomeTask?.id}`)} 
+                    <button
+                        onClick={() => navigate(`hometask/${lessonInfo?.lessonHomeTask?.id}`)}
                         className={cls.card__details__btn}
                     >
                         Vazifa <ArrowRightIcon />
@@ -38,6 +41,10 @@ const LessonInfoCard = ({ lessonId = '' }) => {
                 visible={isOpenVideoModal}
                 urls={[lessonVideoUrl]}
                 setVisible={() => setIsOpenVideoModal(false)}
+            />
+            <MediaMergingDialog
+                isOpen={videoPreview?.isOpen}
+                onClose={() => setVideoPreview({ isOpen: false, status: null, url: '' })}
             />
         </div>
     )
