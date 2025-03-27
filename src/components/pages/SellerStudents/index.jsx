87c5f38ef431b44be74b9289829405a2f4ca70d2
Muxammadaziz <muxammadazizramziddinov@gmail.com>
2025-03-27 11:50:40 +0500

    Seller students filter updated

--- src/components/pages/SellerStudents/index.jsx
+++ src/components/pages/SellerStudents/index.jsx
@@ -1,26 +1,24 @@
-import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import Loader from '@/components/UI/atoms/Loader';
 import useGetSellerStudents from '@/hooks/useSeller';
+import useSessionState from '@/hooks/useSessionState';
 import { PlusIcon } from '@/components/UI/atoms/icons';
 import Button from '@/components/UI/atoms/Buttons/Button';
 import StudentsTable from '@/components/templates/StudentsTable';
 import StudentsSearchBar from '@/components/UI/organisms/StudentsSearchBar';
 import cls from './SellerStudents.module.scss';
-import Loader from '@/components/UI/atoms/Loader';
 
 const SellerStudents = () => {
     const navigate = useNavigate() 
-    const [filter, setFilter] = useState({})
+    const [filter, setFilter] = useSessionState('seller-students-filter', {})
     const { data: students, isLoading: isLoadingStudents } = useGetSellerStudents(filter)
 
     return (
         <div className={cls.page}>
             <div className={cls.page__header}>
                 <StudentsSearchBar
-                    onChangeStatus={(status) => setFilter(state => ({ ...state, status: status?.value }))}
-                    onChangeFirstName={e => setFilter(state => ({ ...state, firstName: e.target.value?.trim() }))}
-                    onChangeLastName={e => setFilter(state => ({ ...state, lastName: e.target.value?.trim() }))}
-                    onChangePhone={phone => setFilter(state => ({ ...state, phone }))}
+                   onChange={setFilter}
+                   defaultValue={filter}
                 />
                 <Button onClick={() => navigate('/sales-form')}>O’quvchi qo’shish <PlusIcon /> </Button>
             </div>
